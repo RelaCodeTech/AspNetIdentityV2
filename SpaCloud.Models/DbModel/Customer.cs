af02ce587324abec96b09e26dd8d8ea7aab99db0
RelacodeTech <relacode.tech@gmail.com>
2015-07-27 10:36:36 +0530

    wip-27jul-10am

--- SpaCloud.Models/DbModel/Customer.cs
+++ SpaCloud.Models/DbModel/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,15 +12,34 @@ namespace SpaCloud.Models.DbModel
         public long CustomerID { get; set; }
         public long CompanyID { get; set; }
         public int CustomerTypeID { get; set; }
+
+        [Display(Name="First name")]
         public string CustomerFname { get; set; }
+
+        [Display(Name = "Last name")]
         public string CustomerLname { get; set; }
+
+        [Display(Name = "Gender")]
         public string CustomerGender { get; set; }
+
+        [Display(Name = "Mobile no")]
         public string CustomerMobileNo { get; set; }
+
+        [Display(Name = "DOB")]
         public Nullable<System.DateTime> CustomerDOB { get; set; }
+
+
+        [Display(Name = "Email ID")]
         public string CustomerEmailID { get; set; }
+
+        [Display(Name = "Alt Mobile no")]
         public string CustomerAltMobileNo { get; set; }
+
+        [Display(Name = "Notes")]
         public string CustomerNotes { get; set; }
         public long CustomerCreatedBranchID { get; set; }
+
+        [Display(Name = "Country")]
         public string CustomerCountry { get; set; }
         public string CustomerCreatedBy { get; set; }
         public string CustomerIsActive { get; set; }
